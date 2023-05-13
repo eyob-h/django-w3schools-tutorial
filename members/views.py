fcb6a959e5c14c718592864769a1bf8d29c6ab76
Eyob <110186516+eyob-h@users.noreply.github.com>
2023-05-13 10:45:46 +0300

    first commit

---
+++ members/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+
+def members(request):
+    mymembers = Member.objects.all().values()
+    template = loader.get_template("all_members.html")
+
+    context = {
+        'mymembers':mymembers,
+    }
+
+    return HttpResponse(template.render(context, request))
+
+
+
+
+
+
+
+def index(request):
+    template = loader.get_template("firstpage.html")
+
+    return HttpResponse(template.render())
